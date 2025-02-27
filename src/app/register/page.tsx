3bc0ca41e016cab159404bdd36abe00273cd0001
Joy43 <ssjoy43@gmail.com>
2025-02-27 19:56:19 +0600

    next ecommarce site login

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import RegisterFrom from '@/components/modules/auth/register/RegisterFrom'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+        <RegisterFrom/>
+      
+    </div>
+  )
+}
+
+export default page
