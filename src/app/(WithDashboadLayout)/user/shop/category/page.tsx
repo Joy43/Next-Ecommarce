4b49efb1e55aab72a5569c40520e813f0237ec1c
Joy43 <ssjoy43@gmail.com>
2025-03-07 16:50:45 +0600

    next ecommarce module

--- src/app/(WithDashboadLayout)/user/shop/category/page.tsx
+++ src/app/(WithDashboadLayout)/user/shop/category/page.tsx
@@ -1,11 +1,14 @@
 import ManageCategory from "@/components/modules/shop/category"
+import { getAllCategories } from "@/services/Category"
 
 
 
-const ProductCategoryPage = () => {
+const ProductCategoryPage = async () => {
+  const {data,meta}=await getAllCategories();
+  console.log(data);
   return (
     <div>
-      <ManageCategory/>
+      <ManageCategory categories={data} />
     </div>
   )
 }
